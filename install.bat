cd82895ce040053195e56f919bb3a8234bc51133
akio0000 <oosaki.akio@gmail.com>
2026-04-15 16:29:13 +0900

    Create run.bat to automate Streamlit app launch
    Add a run.bat script for easier execution of Streamlit app.

--- install.bat
+++ install.bat
@@ -5,4 +5,11 @@ python -m venv venv
 call venv\scripts\activate
 python -m pip install --upgrade pip
 pip install streamlit
+
+echo @echo off > run.bat
+echo cd /d %%~dp0 >> run.bat
+echo call venv\Scripts\activate >> run.bat
+echo streamlit run main.py >> run.bat
+echo pause >> run.bat
+
 streamlit run main.py
